ef87ac01d176a85d1e23a319fbb882cecb50d00b
danbenosem <danielbenjami562@gmail.com>
2026-04-10 22:37:30 +0100

    task

---
+++ assignment/twopointone.py
@@ -0,0 +1,8 @@
+
+""""
+2.1 (What does this code do?) Create the variables x = 2 and y = 3, then determine what
+each of the following statements displays:
+a) print('x =', x) ans: x=2
+b) print('Value of', x, '+', x, 'is', (x + x)) ans: value of 2 +2 is 4
+c) print('x =') ans : x =
+d) print((x + y), '=', (y + x)) ans = 5 = 5
---
+++ assignment/twopointtwo.py
@@ -0,0 +1,6 @@
+
+
+"""
+2.2 (What’s wrong with this code?) The following code should read an integer into the
+variable rating:
+rating = input('Enter an integer rating between 1 and 10') ans : the code is only used to receive values in string
