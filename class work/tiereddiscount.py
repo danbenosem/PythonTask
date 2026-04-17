57b3d3bafe1d4f9c8f468e4fa5f242fdc9dbedb9
danbenosem <danielbenjami562@gmail.com>
2026-04-17 10:12:23 +0100

    class work

---
+++ class work/tiereddiscount.py
@@ -0,0 +1,50 @@
+"""
+DECLARE VARIBLE
+
+collect price from the user
+
+IF price matches the PRICE RANGE.
+CALCULATE DISCOUNT
+
+PRINT DISCOUNT
+
+END
+
+
+
+
+"""
+
+
+user_input= int(input("Enter the price:"))
+
+if user_input>1000 and user_input<10000:
+    print("you have 5% discount")
+    discount_price= 0.05 * user_input
+    final_discount= user_input- discount_price
+    print (f"discount price  is {final_discount}")
+
+elif user_input>10000 and user_input<50000:
+    print("you have 10% discount")
+    discount= 0.10 * user_input
+    final_discount= user_input- discount_price
+    print (f"discount price  is {final_discount}")
+
+elif user_input>50000:
+     print("you have 20% discount")
+     discount= 0.20 * user_input
+     final_discount= user_input- discount_price
+     print (f"discount price  is {final_discount}")
+
+
+
+
+
+
+
+
+
+
+
+
+
