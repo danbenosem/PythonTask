147c8566058ff060855d11d3d7e8d9bf6c48e383
danbenosem <danielbenjami562@gmail.com>
2026-04-20 10:33:42 +0100

    multiplication table

---
+++ class work/multplicationtable.py
@@ -0,0 +1,22 @@
+''''
+
+collect input from the user
+
+do a for loop that counts from 1 to 10
+
+
+for every count the number will be multiplied to
+
+
+print the out put
+'''''
+
+
+user_input = int(input("Enter the number"))
+
+for number in range(1,11):
+   
+    print(f"{user_input} ❎️ {number} = {user_input*number}" )
+    
+
+
--- class work/password.py
+++ class work/password.py
@@ -1,7 +1,15 @@
 """
 START
 USER ENTER THE PASSWORD
-IF 
+IF password length using the len funtion matches the first condition which is less than 8 
+PRINT very weak
+
+ELSE IF password length using the len funtion matches the second condition which is greater than 8 and less than eqaul to 16 
+PRINT strong
+
+ELSE IF password length using the len funtion matches the third condition which is greater than 16 
+PRINT strong 
+
 
 
 
@@ -17,3 +25,12 @@ password = input("Enter the password:")
 
 if len(password) <8:
     print("very weak")
+elif len(password)==8:
+    print("weak")
+
+elif  len(password) >=8 and <=16:
+    print("strong ")
+
+elif  len(password) >16:
+    print(" very strong ")
+
