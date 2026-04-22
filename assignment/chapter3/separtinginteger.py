4077e48a31c1d77c36cb19db4b35446b136b0133
danbenosem <danielbenjami562@gmail.com>
2026-04-22 09:55:50 +0100

    task

--- assignment/chapter3/separtinginteger.py
+++ assignment/chapter3/separtinginteger.py
@@ -1,9 +1,24 @@
+
+
+"""
+define a varible digit
+
+set a container that will collect the digit from back
+
+find a way to collect last digit and collect from a container
+
+continue
+
+till it gets zero
+
+
+"""
 number=12345
 
 new_digit=0
 while number>0:
-    new_digit*=(number%10)+ (number%10)
-    number//10
+    new_digit= (new_digit*10)+ (number%10)
+    number//=10
 
 print(new_digit)
     
